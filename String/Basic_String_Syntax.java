a7b6cbe217f542cd3a89acec26c3b3bb7921cd18
GitanjaliShelke03 <gitshelke@gmail.com>
2025-12-28 14:31:12 +0530

    Basic String program

--- String/Basic_String_Syntax.java
+++ String/Basic_String_Syntax.java
@@ -1,7 +1,8 @@
 public class Basic_String_Syntax{
 public static void main(String[] args){
 
-String s = "Hello world!";
-System.out.println(s);
+String s = "Hello"; //immutable String, obj crerated in String pool
+s.concat("world!"); //creates new obj
+System.out.println(s); //s points "Hello"
  }
 }
