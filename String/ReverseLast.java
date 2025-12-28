fc6aedb1da12426be48e806919793da637a196a0
GitanjaliShelke03 <gitshelke@gmail.com>
2025-12-28 15:40:42 +0530

    Check whether the String is palindrome or not

--- String/ReverseLast.java
+++ String/ReverseLast.java
@@ -1,12 +1,21 @@
 public class ReverseLast{
  public static void main(String[] args){
 
- String s = "Hello dlorw";
+ String s = "Hello dlrow";
 
  String[] words = s.split("\\s+");
 
-  for(int i = words.length-1; i>=0; i--){
- System.out.print(words[i] +" ");
+ String lastWord = words[words.length-1]; 
+
+ String rev = "";
+ 
+  for(int i = lastWord.length()-1; i>=0; i--){
+  rev +=lastWord.charAt(i);
+  }
+  
+ for (int i = 0; i < words.length - 1; i++) { //print Hello only 
+            System.out.print(words[i] + " ");
+        }
+        System.out.print(rev);     
   }
  }
-}
