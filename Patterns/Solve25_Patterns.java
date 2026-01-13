bfb153601a176d65ec118216a66dd91aa661b81d
GitanjaliShelke03 <gitshelke@gmail.com>
2026-01-14 00:46:25 +0530

    Diamond pattern added

--- Patterns/Solve25_Patterns.java
+++ Patterns/Solve25_Patterns.java
@@ -111,5 +111,25 @@ public class Solve25_Patterns {
 	 System.out.println();
 	 }
   }
+  
+   System.out.println("Diamond pattern");
+	 for(int i =1; i<=4;i++) {
+		 for(int j =3; j>=i;j--) {
+			 System.out.print(" ");
+		 }
+		 for(int k = 1; k<=i;k++) {
+			 System.out.print("* ");
+		 }
+		 System.out.println();
+	 }
+	 for(int i =1 ;i<=3; i++) {
+		 for(int s =1 ;s<=i;s++) {
+			 System.out.print(" ");
+		 }
+	   for(int l =3; l>=i ;l--) {
+		 System.out.print("* ");
+	   }
+	  System.out.println();
+	 }
   }
 }
