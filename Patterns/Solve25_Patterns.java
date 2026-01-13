42e3082dc2921bcfaf1af0e4a4fee562677c6bda
GitanjaliShelke03 <gitshelke@gmail.com>
2026-01-13 23:55:05 +0530

    Triangular Pyramid added

--- Patterns/Solve25_Patterns.java
+++ Patterns/Solve25_Patterns.java
@@ -75,5 +75,21 @@ public class Solve25_Patterns {
 		}
 		System.out.println();
      }
+	 
+	System.out.println("Palindrome Triangular");
+	 
+	 for(int i = 1;i<=4;i++) {
+		 for(int s = 4;s>=i;s--) {
+			 System.out.print(" ");
+		 }
+		for(int j =i; j>=1;j--) {
+			System.out.print(j);
+		}
+		for(int k =2 ;k<=i; k++) {
+			System.out.print(k);
+		}
+	 System.out.println();
+	 }
+  }
   }
 }
