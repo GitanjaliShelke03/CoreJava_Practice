067f92ca7c86136f710e432787ff3e7991bfb45f
GitanjaliShelke03 <gitshelke@gmail.com>
2026-01-13 20:35:25 +0530

    updated

--- Patterns/SquarePattern.java
+++ Patterns/SquarePattern.java
@@ -1,18 +1,14 @@
-import java.util.Scanner;
-
+import java.util.Scanner; 
 public class SquarePattern{
-	
 public static void main(String args[]){
- 
- Scanner sc = new Scanner(System.in);
-  System.out.println("Enter number of line to print sqauare:");
-  int line = sc.nextInt();
- for(int i=1; i<=line; i++){
-    for(int ast=1; ast<=line ;ast++){
-	
- System.out.print("* ");
- }
- System.out.println();
+int ast;
+int line;
+
+for(ast=0; ast<=4 ;ast++){
+	for(line=0; line<=4; line++){
+System.out.print("* ");
+   }
+System.out.println();
   }
  }
 }
