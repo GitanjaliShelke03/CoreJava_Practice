e1454908abc656a1152aca746f9f3bd619a511a6
GitanjaliShelke03 <gitshelke@gmail.com>
2026-01-14 00:35:23 +0530

    Number Changing Pyramid added

--- Patterns/Solve25_Patterns.java
+++ Patterns/Solve25_Patterns.java
@@ -76,6 +76,16 @@ public class Solve25_Patterns {
 		 System.out.println();
 	 }
 	 
+	 System.out.println("Number Changing Pyramid");
+	 int num = 1;
+	 for(int i =1; i<=4;i++) {
+		 for(int j =1
+				 ; j<=i;j++) {
+			 System.out.print(num +" ");
+			 num++;
+		 }
+		 System.out.println();
+	 }
 	 
 	  
 	 System.out.println("number triangular");
