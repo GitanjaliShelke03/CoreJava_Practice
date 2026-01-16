8dde3bfb1a51d6717f6f4881cab344f95b4f5777
GitanjaliShelke03 <gitshelke@gmail.com>
2026-01-16 14:42:49 +0530

    Square Hollow Pattern added

--- Patterns/Solve25_Patterns.java
+++ Patterns/Solve25_Patterns.java
@@ -131,5 +131,19 @@ public class Solve25_Patterns {
 	   }
 	  System.out.println();
 	 }
+	 
+	  System.out.println("Square Hollow Pattern");
+	 
+	 for(int i = 1; i <= 5; i++) {
+		 
+		 for(int j = 1; j <=5; j++) {
+			 if(i==1 || j==5 || i==5 || j==1) {
+			 System.out.print("*");
+			 }else {
+				 System.out.print(" ");
+			 }
+		 }
+		 System.out.println();
+	 }
   }
 }
