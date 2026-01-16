b4687a7c9adbad656fe208b4c0f3c468f183453c
GitanjaliShelke03 <gitshelke@gmail.com>
2026-01-16 14:55:21 +0530

    Triangle star pattern added

--- Patterns/Solve25_Patterns.java
+++ Patterns/Solve25_Patterns.java
@@ -112,6 +112,18 @@ public class Solve25_Patterns {
 	 }
   }
   
+   System.out.println("Triangle star pattern");
+	 
+	 for(int i=1; i<=5; i++) {
+		 for(int s= 4; s>=i ; s--) {
+			 System.out.print(" ");
+		 }
+		 for(int j = 1; j<=i; j++) {
+			 System.out.print("* ");
+		 }
+		 System.out.println();
+	 }
+  
    System.out.println("Diamond pattern");
 	 for(int i =1; i<=4;i++) {
 		 for(int j =3; j>=i;j--) {
