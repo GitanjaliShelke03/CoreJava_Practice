7b0234006bd6e9cf1c0e27f892b0cf51d43d7ea1
GitanjaliShelke03 <gitshelke@gmail.com>
2026-01-19 16:10:18 +0530

    hollow triangle added

--- Patterns/Solve25_Patterns.java
+++ Patterns/Solve25_Patterns.java
@@ -158,6 +158,25 @@ public class Solve25_Patterns {
 		 System.out.println();
 	 }
 	 
+	 
+	 
+	 System.out.println("hollow triangle");
+	 for(int i = 1;i<=5;i++) {
+		 for(int s=4;s>=i;s--) {
+			 System.out.print(" ");
+		 }
+		 for(int j=1;j<=2*i-1;j++) {
+			 if(i==5 || j==1 || j==2*i-1 ) {
+			 System.out.print("*");
+			 }
+			 else {
+				 System.out.print(" ");
+			 }
+		 }
+		 System.out.println();
+	 }
+	 
+	 
 	 	 System.out.println("Rhombus Pattern");
 	 for(int i=1; i<=5; i++) {
 		 for(int s =1; s<i; s++) {
