f381fb557c807b28a201f22c173913891329a465
GitanjaliShelke03 <gitshelke@gmail.com>
2026-02-03 22:49:52 +0530

    K pattern

--- Patterns/Solve25_Patterns.java
+++ Patterns/Solve25_Patterns.java
@@ -265,5 +265,20 @@ public class Solve25_Patterns {
 			 System.out.println();
 	 }
 	 
+	 
+	 System.out.println("K pattern");
+	 
+	 for(int i=1; i<=4; i++){
+		 for(int j=4; j>=i; j--){
+			 System.out.print("*");
+		 }
+		  System.out.println();
+	 }
+	 for(int i=1; i<=4; i++){
+		 for(int j=1; j<=i; j++){
+		 System.out.print("*");
+		 }
+		  System.out.println();
+	 }		  
   }
 }
