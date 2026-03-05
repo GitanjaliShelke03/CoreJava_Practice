0f26a4c53cf748798c0105ddd33f8f70a7ec5981
GitanjaliShelke03 <gitshelke@gmail.com>
2026-03-05 17:00:48 +0530

    Reverse Number

---
+++ Arithmetic/RevNum.java
@@ -0,0 +1,13 @@
+public class RevNum{
+	public static void main(String[] args){
+		int a = 1234;
+		int rev =0;
+		
+		while(a>0){
+			int digit = a%10;  //4 3 2 1
+			rev = rev*10+digit;   //4 43 432 4321 
+             a = a/10;        //123 12 1 0
+		}
+		System.out.println(rev);
+	}
+}
