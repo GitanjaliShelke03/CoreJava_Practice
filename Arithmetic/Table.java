25b76614bde1b64e261a30661f2cd28085296e86
GitanjaliShelke03 <gitshelke@gmail.com>
2026-04-15 11:02:44 +0530

    _

---
+++ Arithmetic/Table.java
@@ -0,0 +1,28 @@
+class Table{
+/*
+ public static int t = 2;
+  
+ public static int table =0;
+ //non static variable can not accessible in static context so i created static variable
+ 
+  public static void main(String[] args){
+   for(int i =1; i<=10;i++){
+	   table = t*i;
+	   System.out.println(table);
+   }
+
+  }
+  */
+  
+   public static void main(String[] args){
+      int t = 2;
+  
+      int table =0;
+ 
+   for(int i =1; i<=10;i++){
+	   table = t*i;
+	   System.out.println(table);
+   }
+}
+}
+  
